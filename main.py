92a2bfc98ddfc11ff4746265999dee988d2bdbb4
calmdownbe <128601176+calmdownbe@users.noreply.github.com>
2023-03-22 19:29:05 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import re
+
+print"Email Validator"
+
+email = raw_input("Input the email address to be validated: ")
+
+if not re.match("^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", email):
+    print "Email is not valid."
+else:
+    print "Email is valid."
+
+
